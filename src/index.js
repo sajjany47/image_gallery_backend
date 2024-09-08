d7cb066ec3b64151ac755d7e64507907d8ab4ea9
sajjany47 <sajjany47@gmail.com>
2024-09-08 16:06:29 +0530

    cors error

--- src/index.js
+++ src/index.js
@@ -28,7 +28,10 @@ function main() {
 
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://image-gallery-backend-piw3.onrender.com",
+      ],
       methods: ["GET", "POST"],
       credentials: true,
     })
